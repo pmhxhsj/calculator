667fafb43e8c90cf74ec51848622e993c71fb3c9
pmhxhsj <minho7952@naver.com>
2021-12-20 17:28:54 +0900

    feat: 연산자 상수화

---
+++ src/js/operator.js
@@ -0,0 +1,12 @@
+const OPERATOR = {
+  PLUS: document.querySelector('#calculator-plus-btn').value,
+  MINUS: document.querySelector('#calculator-minus-btn').value,
+  MUTIPLE: document.querySelector('#calculator-multiple-btn').value,
+  DEVIDE: document.querySelector('#calculator-devide-btn').value,
+  REMIND: document.querySelector('#calculator-remind-btn').value,
+
+  DOT: document.querySelector('#calculator-dot-btn').value,
+  ROOT: document.querySelector('#calculator-root-btn').value,
+};
+
+export { OPERATOR };
